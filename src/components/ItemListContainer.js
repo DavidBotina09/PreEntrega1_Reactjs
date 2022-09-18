83f6976ad418df3208eb7bd26d701b1efe8ae3eb
DavidBotina09 <davidbotinasc@gmail.com>
2022-09-18 16:58:01 -0500

    PreEntrega1_DavidBotina

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,11 @@
+import './ItemListContainer.css';
+const ItemListContainer = ({ greeting }) => {
+    return (
+        <>
+        <h1>Productos</h1>
+        <h3 className="greeting">{greeting}</h3>
+        </>
+    );
+}
+ 
+export default ItemListContainer;
